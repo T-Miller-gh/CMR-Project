29b4ca6da86b3fc7699127e70d4d5f426c464f5e
solly <sa144385@umconnect.umt.edu>
2023-11-29 21:35:51 -0700

    started horse wandering script
    - definitely needs some fine tuning, horses only rotate right now

---
+++ Assets/Scripts/horseWanderAI.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horseWanderAI : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float rotSpeed = 100f;
+
+    private bool isWandering = false;
+    private bool isRotatingRight = false;
+    private bool isRotatingLeft = false;
+    private bool isWalking = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+
+        if (isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * moveSpeed);
+        }
+
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * -moveSpeed);
+        }
+
+        /*
+        if(isWalking == true)
+        {
+            transform.position += transform.forward * moveSpeed;
+        }
+        */
+    }
+
+    IEnumerator Wander()
+    {
+        // rot time is amount of time capsule will be rotating
+        int rotTime = Random.Range(1, 3);
+        // rot wait is amount of time between capsule rotations
+        int rotWait = Random.Range(1, 4);
+        // this determines whether or not it is going to rotate left or right
+        int rotateLorR = Random.Range(1, 2);
+        // amount of time between walking
+        int walkWait = Random.Range(1, 4);
+        // amount of time it will be walking
+        int walkTime = Random.Range(1, 5);
+
+        isWandering = true;
+
+        // gets random walk time from above
+        yield return new WaitForSeconds(walkWait);
+        // turns walking on 
+        isWalking = true;
+        // starts walking timer 
+        yield return new WaitForSeconds(walkTime);
+        // turns walking off after timer is done
+        isWalking = false;
+        // seconds before ai rotates
+        yield return new WaitForSeconds(rotWait);
+
+        if (rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        // stops AI from wandering again so the cycle can start all over (in fixed update)
+        isWandering = false;
+    }
+}
