a0867864c9a955f0b8b88dc82164bfbdd8a0a666
solly <75548882+solomonalbertsongore@users.noreply.github.com>
2024-01-30 16:05:17 -0700

    fog updating as game progresses fixed

--- Assets/Scripts/modifyFog.cs
+++ Assets/Scripts/modifyFog.cs
@@ -4,35 +4,24 @@ using UnityEngine;
 
 public class modifyFog : MonoBehaviour
 {
+    public characterManager characterManager;
+
     // Adjust these via the Inspector in Unity
+    [SerializeField] private float startDensity = 0.02f; 
     [SerializeField] private float targetDensity = 0.2f;
 
-    //[Tooltip(2Fade duration in seconds)]
-    [SerializeField] private float fadeDuration = 300.0f;
-
-    private bool fogTriggered = false;
-
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (!fogTriggered)
-        {   
-            //So it will only trigger once per play
-            fogTriggered = true;
-            StartCoroutine(FadeFog());
-        }      
+        updateFog(); 
     }
 
-    private IEnumerator FadeFog()
+    void updateFog()
     {
-        var timePassed = .01f;
-        while (timePassed <= fadeDuration)
+        if(characterManager.timerOn)
         {
-            //Increase fog from current density to target density over total duration
-            var factor = timePassed / fadeDuration;
-            RenderSettings.fogDensity = Mathf.Lerp(0.02f, targetDensity, factor);
-            timePassed += .001f;
-
-            yield return null;
+            float newFogDensity = Mathf.Lerp(startDensity, targetDensity, characterManager.gameProgress);
+            RenderSettings.fogDensity = newFogDensity;
+            Debug.Log(newFogDensity); 
         }
     }
 }
